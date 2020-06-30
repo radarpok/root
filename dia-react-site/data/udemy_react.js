3400ef19544921219d1ed11c1162453c7a2fefc1
dio <vdiana.m8@gmail.com>
2020-07-01 00:36:02 +0200

    fault commit

---
+++ dia-react-site/data/udemy_react.js
@@ -0,0 +1,34 @@
+module.exports._ = {
+    // here should come the chapters
+    section_1: {
+        id: 1,
+        title: "Introduction to React",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_2: {
+        id: 2,
+        title: "JSX and React Components",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_: {
+        title: "State and Props",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_: {
+      
+        title: "State and Props",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    } 
+}
