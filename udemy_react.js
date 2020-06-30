ee8ff379020a34ff9ad63fdc08f947c16413d824
dio <vdiana.m8@gmail.com>
2020-06-30 23:32:19 +0200

    initial commit

---
+++ udemy_react.js
@@ -0,0 +1,34 @@
+module.exports.udemy_react_sections = {
+    // here should come the chapters
+    section_1: {
+        id: 1,
+        title: "Introduction to React",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_2: {
+        id: 2,
+        title: "2. JSX and React Components",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_: {
+        title: "3. State and Props",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    },
+    section_: {
+      
+        title: "3. State and Props",
+        sub_chapters: {
+            sub_section_1: {},
+            sub_section_2: {}
+        },
+    } 
+}
---
+++ vanKedDolgozni.js
@@ -0,0 +1,21 @@
+console.log("commands.js");
+
+
+//const dolgoz =()=>{};
+
+function dolgoz(_){
+    if(_ === 1){
+        console.log("van kedvem dolgozni")
+    }else{
+        console.log("nincs kedvem csinálni semmit")
+    }
+} 
+
+var today = "tuesday";
+if(today === "tuesday"){
+    console.log("ma kedd van, bizzuk az időjárásra a munkakedvünket");
+    
+    let vankedvMero= 1;
+
+    dolgoz(vankedvMero)
+}
