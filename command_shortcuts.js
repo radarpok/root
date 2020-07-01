b5d0845e7b30f4f5056486c186f0280ed053472c
dio <vdiana.m8@gmail.com>
2020-07-01 17:07:42 +0200

    creating command shortcuts js object

---
+++ command_shortcuts.js
@@ -0,0 +1,12 @@
+module.exports.command_shortcuts=
+{
+    linux: {
+        touch: "making a new file"
+    },
+    vs_code:{},
+    total_commander:{},
+    excel:{},
+    word:{},
+    mindmanager:{},
+    github:{}
+}
