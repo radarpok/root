72e104f387fe2e0002657cac67eb4b4f89d66127
practicalli <github@practical.li>
2020-07-03 00:25:34 +0200

    adding shortcuts for emacs buffer

--- command_shortcuts.js
+++ command_shortcuts.js
@@ -8,5 +8,14 @@ module.exports.command_shortcuts=
     excel:{},
     word:{},
     mindmanager:{},
-    github:{}
+    github:{},
+    windows:{},
+    linux:{},
+    mozilla:{},
+    firefox:{},
+    emacs: {
+        ctrl_x_b: "select or create a buffer named buffer (switch-to-buffer)",
+        ctrl_x_4_b: "Similar, but select buffer in another window (switch-to-buffer-other-window)",
+        ctrl_x_5_b: "Similar, but select buffer in a separate frame (switch-to-buffer-other-frame)"
+    }
 }
