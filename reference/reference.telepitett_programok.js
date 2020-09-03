51764639ebf1f33edd3f79bfaaa69224da65c6f4
radarpok <vdiana.m8@gmail.com>
2020-09-03 19:40:25 +0200

    updating program installation guides in reference

--- reference/reference.telepitett_programok.js
+++ reference/reference.telepitett_programok.js
@@ -45,12 +45,31 @@ const installed_programs_packages_libraries = {
         libre_office: "libre office",
         android_studio: "android studio",
         team_viewer: "team viewer",
-        atom: "atom",
+        atom: {
+            name: "Atom",
+            install: "sudo snap install atom --classic",
+            uninstall: "sudo snap remove atom"
+        },
         netbeans: "netbeans",
         jvm: "jvm",
-        clojure: "clojure",
+        clojure: {
+            name: "clojure",
+            install: [{
+                1: "curl -O https://download.clojure.org/install/linux-install-1.10.1.561.sh",
+                2: "chmod +x linux-install-1.10.1.561.sh",
+                3: "sudo ./linux-install-1.10.1.561.sh",
+            }],
+            uninstall: "sudo apt-get purge --auto-remove clojure "
+        },
         julia: "julia",
-        racket: "racket",
+        racket: {
+            name: "racket",
+            install: [{
+                1: "sudo add-apt-repository ppa:plt/racket",
+                2: "sudo apt-get update",
+                3: "sudo apt-get install racket"
+            }]
+        },
         npm: "npm",
 	    leiningen: {
             name: "leiningen",
@@ -101,16 +120,17 @@ const installed_programs_packages_libraries = {
                 // latest for ubuntu on 2020.07.18. is "redshift-1.12.tar.gz"
                 // needed for later configurations
             dependencies: {
-                intltool,
+                intltool :{
                     install: "sudo apt install intltool",
-                randr,
+                    },
+                randr: {
                     install: {
                         1: "cd in terminal to the downloaded and extracted folder of redshift",
                         2: "./configure --enable-randr --enable-gui --enable-ubuntu --with-systemduserunitdir=$HOME/.config/systemd/user",
                         3: "make",
                         4: "sudo make install",
                             },
-                        },
+                    },
             install: "sudo apt-get install redshift",
             configure_light_temperaure: {
                 1: "locate longitude and latitude manually or via geoclude https://location.services.mozilla.com/v1/geolocate?key=geoclue",
@@ -126,13 +146,89 @@ const installed_programs_packages_libraries = {
                 5: "chmod +x ~/.config/redshift/hooks/brightness.sh",
                     // making it executable
                 6: "turn on autostart"
-            }
-            
-                
-
+            },
+        }
         }
     },
-    windows:{}
+    windows:{
+        chrome: {
+            name: "Google Chrome",
+            download: "https://www.google.com/intl/hu_hu/chrome/"
+        },
+        total_commander: {
+            name: "Total Commander",
+            download: "https://www.totalcommander.hu/letoltes"
+        },
+        teamviewer: {
+            name: "TeamViewer",
+            download: "https://www.teamviewer.com/hu/?gclid=EAIaIQobChMI6fH53-275gIVy-R3Ch3qPA5pEAAYASAAEgI3xvD_BwE"
+        },
+        vscode: {
+            name: "Visual Studio Code",
+            download: "https://code.visualstudio.com/"
+        },
+        mindmanager: {
+            name: "Mindjet MindManager 2016 16.0.152",
+            download: "https://thepiratebay.org/description.php?id=12609825"
+        },
+        esetnod32: {
+            name: "Eset Nod32",
+            download: "https://www.eset.com/hu/otthoni/antivirus/letoltes/",
+            // need to be renewed monthly but it's always updated
+            license_keys: "https://nod32-trial-keys.site/",
+        },
+        utorrent: {
+            name: "μTorrent",
+            download: "https://www.utorrent.com/downloads/win"
+        },
+        discord: {
+            name: "Discord",
+            download: "https://discordapp.com/download"
+        },
+        winrar: {
+            name: "WinRar",
+            download: "https://drive.google.com/open?id=13qb7J4nQ1Lmkipx0MIjSeuIAxcWRsP1L"
+        },
+        skype: {
+            name: "Skype",
+            download: "https://www.skype.com/hu/get-skype/"
+        },
+        photoshop: {
+            name: "Adobe Photoshop CC 2015 Portable",
+            description: "best designing tool so far",
+            download: "https://drive.google.com/open?id=1v6vkjxCT0S3yFpeoE6ftem0MMJ5aZbZY"
+        },
+        after_effects: {
+            name: "Adobe After Effects 2019",
+            description: "for making visual effects and animated graphics",
+            download: "https://mega.nz/#F!EFo1RagA!LVZRSHfTq-0cD9x-lK-PRg"
+        },
+        illustrator: {
+            name: "Adobe Illustrator CC 2019",
+            description: "vectorgraphical editor",
+            download: "https://drive.google.com/open?id=16aFr2QFrboe6_Kfon8dXRdG89oJgIS4t"
+        },
+        vegas_pro: {
+            name: "Magix Vegas Pro",
+            description: "professional multimedia editor",
+            download: "https://mega.nz/#!JUIFkaCC!4zrcFojzysHKKlRDMczHjo5723IASTr1aZ9cii32Xdw"
+        },
+        flstudio: {
+            name: "FL Studio",
+            description: "digital audio and dj workstation",
+            download: "https://mega.nz/#F!pcJV2QZZ!pwf694UTaQ2j3U1oP5xsFw"
+        },
+        thunderbird: {
+            name: "Thunderbird",
+            description: "my favorite mail client",
+            download: "https://www.thunderbird.net/en-GB/"
+        },
+        movavi: {
+            name: "Movavi Video Converter 19 Premium",
+            description: "movie extension converter",
+            download: "https://drive.google.com/open?id=1wMB1xnDRcV3Vtu25MfxrN1ST5iurU8HX"
+        }
+    }
 }
 
 export default installed_programs_packages_libraries;
